2b450ccae84d69a429e233da88e5a0687db6532d
tpedrajas <tpedrajas@wcs.com>
2023-02-24 17:43:11 +0100

    Add NumberCulturedFormatedAttribute Tests

---
+++ GPM.CubeIntersector.Test/NumberCulturedFormatedAttributeTest.cs
@@ -0,0 +1,180 @@
+﻿namespace GPM.CubeIntersector.Test;
+
+[TestClass]
+public class NumberCulturedFormatedAttributeTest
+{
+
+    #region IsValid tests
+
+    [TestMethod]
+    public void IsValid_Test01()
+    {
+        NumberCulturedFormatedAttribute attribute = new();
+        string checkedValue = "1,23";
+
+        bool result = attribute.IsValid(checkedValue);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void IsValid_Test02()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
+        NumberCulturedFormatedAttribute attribute = new();
+        string checkedValue = "1.23";
+
+        bool result = attribute.IsValid(checkedValue);
+
+        Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("es-ES");
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void IsValid_Test03()
+    {
+        NumberCulturedFormatedAttribute attribute = new();
+        string checkedValue = "01,23";
+
+        bool result = attribute.IsValid(checkedValue);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void IsValid_Test04()
+    {
+        NumberCulturedFormatedAttribute attribute = new();
+        string checkedValue = "0";
+
+        bool result = attribute.IsValid(checkedValue);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void IsValid_Test05()
+    {
+        NumberCulturedFormatedAttribute attribute = new();
+        string checkedValue = "-2,333333";
+
+        bool result = attribute.IsValid(checkedValue);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void IsValid_Test06()
+    {
+        NumberCulturedFormatedAttribute attribute = new();
+        string checkedValue = "-2,";
+
+        bool result = attribute.IsValid(checkedValue);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void IsValid_Test07()
+    {
+        NumberCulturedFormatedAttribute attribute = new();
+        string checkedValue = "-12,0000";
+
+        bool result = attribute.IsValid(checkedValue);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void IsValid_Test08()
+    {
+        NumberCulturedFormatedAttribute attribute = new();
+        string checkedValue = ",1";
+
+        bool result = attribute.IsValid(checkedValue);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void IsValid_Test09()
+    {
+        NumberCulturedFormatedAttribute attribute = new();
+        string checkedValue = "-";
+
+        bool result = attribute.IsValid(checkedValue);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void IsValid_Test10()
+    {
+        NumberCulturedFormatedAttribute attribute = new();
+        string checkedValue = "--1";
+
+        bool result = attribute.IsValid(checkedValue);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void IsValid_Test11()
+    {
+        NumberCulturedFormatedAttribute attribute = new();
+        string checkedValue = "+2";
+
+        bool result = attribute.IsValid(checkedValue);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void IsValid_Test12()
+    {
+        NumberCulturedFormatedAttribute attribute = new();
+        string checkedValue = "aaa";
+
+        bool result = attribute.IsValid(checkedValue);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void IsValid_Test13()
+    {
+        NumberCulturedFormatedAttribute attribute = new();
+        string checkedValue = "a1,1";
+
+        bool result = attribute.IsValid(checkedValue);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void IsValid_Test14()
+    {
+        NumberCulturedFormatedAttribute attribute = new();
+        string checkedValue = "1,,1";
+
+        bool result = attribute.IsValid(checkedValue);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void IsValid_Test15()
+    {
+        NumberCulturedFormatedAttribute attribute = new();
+        string checkedValue = "1,01a";
+
+        bool result = attribute.IsValid(checkedValue);
+
+        Assert.IsFalse(result);
+    }
+
+    #endregion
+
+}
--- GPM.CubeIntersector.Test/Properties/Global.cs
+++ GPM.CubeIntersector.Test/Properties/Global.cs
@@ -2,11 +2,19 @@
 
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+global using System.Globalization;
+
 #endregion
 
 #region usings from Business Logic Layer
 
 global using GPM.CubeIntersector.Domain;
 global using GPM.WPF.Model;
+
+#endregion
+
+#region usings from Support Layer
+
+global using GPM.Product.Common.Validation;
 
 #endregion
